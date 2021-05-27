b8907be5ecdfdf1998ddb4a6ac301d3ec963b815
Antonio Martínez <77786183+antmartinez68@users.noreply.github.com>
2021-05-27 22:12:48 +0200

    Añadidas pruebas función suma

---
+++ isa_calculator_test.py
@@ -0,0 +1,53 @@
+"""Tests para el módulo calculadora"""
+import unittest
+import isa_calculator
+
+
+class PyCalculatorTest(unittest.TestCase):
+    #
+    # Bloque de tests función suma
+    #
+
+    def test_suma_dos_positivos(self):
+        # Arrange
+        value_1 = 2
+        value_2 = 3
+        expected = 5
+        # Act
+        result = isa_calculator.add(value_1, value_2)
+        # Assert
+        self.assertEqual(expected, result)
+
+    def test_suma_dos_negativos(self):
+        # Arrange
+        value_1 = -2
+        value_2 = -3
+        expected = -5
+        # Act
+        result = isa_calculator.add(value_1, value_2)
+        # Assert
+        self.assertEqual(expected, result)
+
+    def test_suma_positivo_negativo(self):
+        # Arrange
+        value_1 = 2
+        value_2 = -3
+        expected = -1
+        # Act
+        result = isa_calculator.add(value_1, value_2)
+        # Assert
+        self.assertEqual(expected, result)
+
+    def test_suma_negativo_positivo(self):
+        # Arrange
+        value_1 = -2
+        value_2 = 3
+        expected = 1
+        # Act
+        result = isa_calculator.add(value_1, value_2)
+        # Assert
+        self.assertEqual(expected, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
